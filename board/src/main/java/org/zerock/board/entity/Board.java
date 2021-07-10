a4588f94da72cd4dee0e303c51d42d1bdd60eef0
DESKTOP-AHG7IDR\cchan <citron03@naver.com>
2021-07-10 22:09:13 +0900

    여러개의 entity를 갖는 예제. 게시글, 유저 정보, 댓글

---
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,28 @@
+package org.zerock.board.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer") // @ToString은 항상 exclude
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    private Member writer; // 연관관계 지정
+    // 맴버중에 라이터
+
+}
