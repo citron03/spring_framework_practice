b968429261a86af35118f132b8e8ebee417cded7
DESKTOP-AHG7IDR\cchan <citron03@naver.com>
2021-07-19 21:34:40 +0900

    쿼리 수정. 리뷰의 쿼리문 추가.

--- mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -8,6 +8,7 @@ import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -41,6 +42,30 @@ public class ReviewRepositoryTests {
             reviewRepository.save(movieReview);
 
         });
+
+    }
+
+
+    @Test
+    public void testGetMovieReviews(){
+
+        // Review 클레스의 Member의 fetch가 Lazy면 한번에 하나의 Review객체와 Member 객체를 조회할 수 있어 오류가 발생한다.
+
+        Movie movie = Movie.builder()
+                .mno(5L)
+                .build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+
+            System.out.println("\t"+movieReview.getGrade());
+            System.out.println("\t"+movieReview.getText());
+            System.out.println("\t"+movieReview.getMember().getEmail());
+            System.out.println("---------------------------------");
+
+        });
+
 
     }
 
