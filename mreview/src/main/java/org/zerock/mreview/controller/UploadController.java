819b83c19eef162952f99046579cea8c2b21ff5a
DESKTOP-AHG7IDR\cchan <citron03@naver.com>
2021-07-22 20:34:07 +0900

    업로드시 섬네일 추가. 업로드한 이미지 삭제 기능 구현.

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,6 +2,7 @@ package org.zerock.mreview.controller;
 
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -68,8 +70,20 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try{
+                // 원본 이미지 저장
                 uploadFile.transferTo(savePath); // 실제 이미지 저장
+
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator
+                        + "s_" + uuid + "_" + fileName;
+                // 섬네일의 파일 이름은 중간에 s_로 시작한다.
+
+                File thumbnailFile = new File(thumbnailSaveName);
+                // 섬네일 생성
+
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
                 resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
+
             } catch (IOException e){
                 e.printStackTrace();
             }
@@ -127,6 +141,32 @@ public class UploadController {
         }
 
         return result;
+    }
+
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(String fileName){
+
+        String srcFileName = null;
+
+        try{
+            srcFileName = URLDecoder.decode(fileName,"UTF-8");
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            boolean result = file.delete();
+
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 
--- mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
+++ mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -13,7 +13,6 @@ public class UploadResultDTO {
 
     private String fileName;
     private String uuid;
-
     private String folderPath;
 
     public String getImageURL(){
@@ -21,6 +20,17 @@ public class UploadResultDTO {
         try{
             return URLEncoder.encode(folderPath+"/"+uuid+"_"+fileName, "UTF-8");
         }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public String getThumbnailURL(){
+
+        try{
+            return URLEncoder.encode(folderPath+"/s_"+uuid+"_"+fileName,"UTF-8");
+        } catch (UnsupportedEncodingException e){
             e.printStackTrace();
         }
         return "";
