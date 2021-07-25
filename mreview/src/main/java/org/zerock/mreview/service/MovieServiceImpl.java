ce2acdd05f197545434a8bd48eb727ca6183329a
DESKTOP-AHG7IDR\cchan <citron03@naver.com>
2021-07-25 22:24:08 +0900

    list 페이지를 구현한다. 리뷰 수, 평균 점수, 수정날짜 번호와 섬네일, 제목이 모두 잘 표시됨을 확인했다.

--- mreview/src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
+++ mreview/src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -2,16 +2,23 @@ package org.zerock.mreview.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
+import org.zerock.mreview.dto.PageResultDTO;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 import org.zerock.mreview.repository.MovieImageRepository;
 import org.zerock.mreview.repository.MovieRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 
 @Service
@@ -40,5 +47,25 @@ public class MovieServiceImpl implements MovieService {
         });
 
         return movie.getMno();
+    }
+
+    @Override
+    public PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("mno").descending());
+
+        Page<Object[]> result = movieRepository.getListPage(pageable);
+
+        // 데이터베이스에서 임의로 이미지를 삭제하여 오류가 발생하였었다.
+        // 이미지에 할당된 영화의 번호에 데이터가 존재하지 않아 오류가 발생.
+
+        Function<Object[], MovieDTO> fn = (arr -> entitiesToDTO(
+                (Movie) arr[0] ,
+                (List<MovieImage>) (Arrays.asList((MovieImage)arr[1])),
+                (Double) arr[2],
+                (Long) arr[3]
+
+        ));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
